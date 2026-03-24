01000d1f272264594c5d4054eae1476a602b20cc
Surudo <1479264526677241-Surudo@noreply.coze.cn>
2026-03-25 02:39:37 +0800

    feat: update logo to hollow folder outline with filled Ñ letter
    - Changed from filled folder to hollow stroke style
    - Kept filled Ñ letter inside
    - Color: #D83A30 (brick red)
    - Regenerated all PNG icons

--- entrypoints/newtab/App.tsx
+++ entrypoints/newtab/App.tsx
@@ -10,16 +10,18 @@ import { useRandomWord, useData, savedAtom, languageAtom } from './hooks/use-dat
 import { useSettings, pronunciationAtom } from './hooks/use-settings';
 import { LANGUAGES } from './types';
 
-// Logo SVG component - folder with Ñ (Spanish letter)
+// Logo SVG component - folder outline with Ñ (Spanish letter)
 const Logo = () => (
   <svg width="28" height="28" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-    {/* Folder shape with top-left notch */}
+    {/* Folder outline (hollow stroke style) */}
     <path 
       d="M14 38C14 34.134 17.134 31 21 31H42L50 23H72L80 31H107C110.866 31 114 34.134 114 38V106C114 109.866 110.866 113 107 113H21C17.134 113 14 109.866 14 106V38Z" 
-      fill="currentColor"
+      stroke="currentColor" 
+      strokeWidth="6"
+      fill="none"
     />
-    {/* Letter Ñ in white */}
-    <g fill="#FFFFFF">
+    {/* Letter Ñ (filled) */}
+    <g fill="currentColor">
       <rect x="38" y="52" width="10" height="38"/>
       <rect x="80" y="52" width="10" height="38"/>
       <polygon points="80,52 90,52 48,90 38,90"/>
@@ -27,7 +29,7 @@ const Logo = () => (
     {/* Tilde ~ above N */}
     <path 
       d="M46 46Q54 40 62 46Q70 52 82 44" 
-      stroke="#FFFFFF" 
+      stroke="currentColor" 
       strokeWidth="5" 
       strokeLinecap="round"
       fill="none"
